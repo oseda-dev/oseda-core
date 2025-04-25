9d1ba3873e18c8b957d64b7c8904634ff434aee6
ReeseHatfield <hatfield.69@wright.edu>
2025-04-24 22:33:55 -0400

    functioning deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# assuming running from oseda core and you have this in another repo relative to this one
+OSEDA_LIB_DIR="../oseda-lib"
+
+if [ "$1" = "--send-repos" ]; then
+    echo "Pushing library to github server"
+    cd "$OSEDA_LIB_DIR"
+    # will push with you local git setup
+    git add --all
+    git commit -m "$(date)"
+    git push
+fi
+
+echo "sshing..."
 set -e
 
 # hope your conf has this exact setup lol
@@ -9,16 +23,17 @@ kill $(pgrep node)
 kill $(pgrep npm)
 
 cd oseda-lib
+cd courses
 git pull
 
 for dir in */; do
     cd "$dir"
     npm install
-    npx run build
+    npx vite build
     cd ..
 done
 
-cd ..
+cd $HOME
 
 cd oseda-core
 git pull
