2cd878a7de3ecaaa4157c6e08b11ad81c7e91036
ReeseHatfield <hatfield.69@wright.edu>
2025-05-05 22:08:44 -0400

    action test

--- pull-oseda-lib.sh
+++ pull-oseda-lib.sh
@@ -24,6 +24,7 @@ cd oseda-core
 
 # complete restart server to push new course material :/
 # this is currently ripped from the deploy script
+touch got-here.txt
 kill $(pgrep node)
 kill $(pgrep npm)
 
