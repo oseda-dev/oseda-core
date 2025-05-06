948ac0f26057ab0f86be0c5c62cd4aa4a666028a
ReeseHatfield <hatfield.69@wright.edu>
2025-05-05 21:54:15 -0400

    add pull script

---
+++ pull-oseda-lib.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+cd ..
+cd oseda-lib
+git pull
+touch temp_file.txt
+
+
+#
+# cd oseda-lib
+# cd courses
+# git pull
+
+# # TODO remove this when send-repo is fixedg
+# for dir in */; do
+#     cd "$dir"
+#     npm install
+#     npx vite build
+#     cd ..
+# done
