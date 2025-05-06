a6e974b7c19b2e88ab4b402a587400f64821c31b
ReeseHatfield <hatfield.69@wright.edu>
2025-05-06 08:25:47 -0400

    action test

--- pull-oseda-lib.sh
+++ pull-oseda-lib.sh
@@ -29,16 +29,16 @@ kill $(pgrep node)
 kill $(pgrep npm)
 
 
-cd $HOME
+# cd $HOME
 
-cd oseda-core
-git pull
+# cd oseda-core
+# git pull
 
-cd backend
-npm install
-nohup ./run-backend.sh > backend.log 2>&1 &
-cd ..
+# cd backend
+# npm install
+# nohup ./run-backend.sh > backend.log 2>&1 &
+# cd ..
 
-cd frontend
-npm install
-nohup ./run-frontend-actually.sh > frontend.log 2>&1 &
+# cd frontend
+# npm install
+# nohup ./run-frontend-actually.sh > frontend.log 2>&1 &
