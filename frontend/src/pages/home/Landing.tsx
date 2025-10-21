9a62ef78fe34efd3915b98595f927028216cc5e0
OfficeCoffee <rosetaylor45342@gmail.com>
2025-10-21 10:31:29 -0400

    Landing page prototype

--- frontend/src/pages/home/Landing.tsx
+++ frontend/src/pages/home/Landing.tsx
@@ -1,29 +1,31 @@
+import "./Landing.css";
 import { useEffect, useState } from "react";
 import NavButton from "../../components/NavButton/NavButton";
 import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
   interface ResType {
-    message: String;
+    message: string;
   }
 
   const [data, setData] = useState<ResType>();
+  const navigate = useNavigate();
+
   useEffect(() => {
     fetch("/api/state")
-      .then((res) => {
-        return res.json();
-      })
-      .then((tmpData) => {
-        setData(tmpData);
-      });
+      .then((res) => res.json())
+      .then((tmpData) => setData(tmpData));
   }, []);
+
   return (
-    <>
+    <div className="landing-container">
       <h1>Landing Page</h1>
-      <div> deploy test 1</div>
+      <div>Deploy test 1</div>
       <div>Data: {data?.message}</div>
-      <NavButton text={"Go to Courses!"} url={"/courses"} />
-    </>
+      <div className="nav-button">
+        <NavButton text="Go to Courses!" url="/courses" />
+      </div>
+    </div>
   );
 };
 
