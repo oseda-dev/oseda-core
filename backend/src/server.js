c0994dfdb8884207b3663983bd844ca3a921c1c7
ReeseHatfield <reesehatfield02@gmail.com>
2026-01-21 20:45:16 -0500

    remove config stuff from server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const fs = require("fs");
 const hosts = require("./hosts");
 const courses = require("./courses");
 
@@ -39,37 +38,17 @@ const buildOsedaExpressServer = (COURSES_ROOT) => {
 
     // list all courses
     // need to add author support eventually as well here
-    server.get("/api/courses", (req, res) => {
-        try {
-            const projects = fs
-                .readdirSync(COURSES_ROOT)
-                .filter((name) =>
-                    fs.statSync(path.join(COURSES_ROOT, name)).isDirectory()
-                );
-            res.json(projects);
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ error: "Failed to load courses" });
-        }
-    });
+    server.get("/api/courses", courses.serveAllCourses(COURSES_ROOT));
 
     // load oseda-config.json for a particular course via query params
     server.get("/api/info", (req, res) => {
         const title = req.query.title;
-        if (!title) return res.status(400).json({ error: "No title provided" });
-
-        const configPath = path.join(COURSES_ROOT, title, "oseda-config.json");
-
-        if (!fs.existsSync(configPath)) {
-            return res.status(404).json({ error: "Course not found" });
-        }
 
         try {
-            const config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+            const config = config.getCourseConfig(title);
             res.json(config);
         } catch (err) {
-            console.error(err);
-            res.status(500).json({ error: "Failed to read config" });
+            res.status(err.status || 500).json({ error: err.message || "Unknown error" });
         }
     });
 
