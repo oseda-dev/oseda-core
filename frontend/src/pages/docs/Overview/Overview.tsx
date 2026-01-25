c820ff9098b63c09bb21a67fe70a58478a40e52f
ReeseHatfield <reesehatfield02@gmail.com>
2026-01-24 21:40:33 -0500

    hit backend

--- frontend/src/pages/docs/Overview/Overview.tsx
+++ frontend/src/pages/docs/Overview/Overview.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import "../Docs.css"
 
 const OverviewDoc = () => {
-    
+
+    const [content, setContent] = useState("");
+
+    useEffect(() => {
+        fetch("/api/docs/README.md")
+            // comes back as raw text
+            .then(res => res.text())
+            .then(text => {
+                setContent(text);
+                console.log(text);
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    }, [])
+
+
     return (
         <div>
             <h1>Overview</h1>
