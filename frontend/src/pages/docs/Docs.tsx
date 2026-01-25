144b8b4bf7708c75d08e48ac63429e38903f1f9a
ReeseHatfield <reesehatfield02@gmail.com>
2026-01-24 20:25:00 -0500

    fix titles in docs

--- frontend/src/pages/docs/Docs.tsx
+++ frontend/src/pages/docs/Docs.tsx
@@ -2,43 +2,47 @@ import React, { useRef, useState } from "react"
 import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar"
 import MarkdownRenderer from "../../components/MarkdownRenderer/MarkdownRenderer"
 import GlassPanel from "../../components/GlassPanel/GlassPanel"
+// import 'react-pro-sidebar/dist/css/styles.css';
 import "./Docs.css";
-
 const Docs: React.FC = () => {
+
 
     return (
         <div className="docs-container">
-                <Sidebar>
-                    <Menu>
-                        <MenuItem>Overview</MenuItem>
+            <Sidebar
+                rootStyles={{
+                    color: '#000000ff',
+                }}            >
+                <Menu style={{ color: "black" }}>
+                    <MenuItem>Overview</MenuItem>
 
-                        <SubMenu title="Contributing">
-                            <MenuItem>Getting Started</MenuItem>
-                            <MenuItem>Guidelines</MenuItem>
-                        </SubMenu>
+                    <SubMenu label="Contributing">
+                        <MenuItem>Getting Started</MenuItem>
+                        <MenuItem>Guidelines</MenuItem>
+                    </SubMenu>
 
-                        <SubMenu title="oseda-cli">
-                            <MenuItem>Cargo</MenuItem>
-                            <MenuItem>Commands</MenuItem>
-                        </SubMenu>
+                    <SubMenu label="oseda-cli">
+                        <MenuItem>Cargo</MenuItem>
+                        <MenuItem>Commands</MenuItem>
+                    </SubMenu>
 
-                        <SubMenu title="oseda-core">
-                            <MenuItem>Frontend</MenuItem>
-                            <MenuItem>Backend</MenuItem>
-                        </SubMenu>
+                    <SubMenu label="oseda-core">
+                        <MenuItem>Frontend</MenuItem>
+                        <MenuItem>Backend</MenuItem>
+                    </SubMenu>
 
-                        <SubMenu title="oseda-lib">
-                            <MenuItem>API</MenuItem>
-                            <MenuItem>Internals</MenuItem>
-                        </SubMenu>
+                    <SubMenu label="oseda-lib">
+                        <MenuItem>API</MenuItem>
+                        <MenuItem>Internals</MenuItem>
+                    </SubMenu>
 
-                        <MenuItem>Readme</MenuItem>
-                    </Menu>
-                </Sidebar>
+                    <MenuItem>Readme</MenuItem>
+                </Menu>
+            </Sidebar>
 
             <GlassPanel
                 as="main"
-                style={{ flex: 1, padding: "2rem" }}
+                style={{ color: "black", flex: 1, padding: "2rem" }}
             >
                 <h1>OSEDA Docs</h1>
                 <MarkdownRenderer markdown={"# Hello"} />
