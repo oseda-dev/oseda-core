955af02456e044d13d8233d42bf6f56a69b77406
ReeseHatfield <reesehatfield02@gmail.com>
2026-01-24 21:30:06 -0500

    fix overview styles

--- frontend/src/pages/docs/Overview/Overview.tsx
+++ frontend/src/pages/docs/Overview/Overview.tsx
@@ -1,8 +1,13 @@
-const OverviewDoc = () => (
-    <>
-        <h1>Overview</h1>
-        <p>OSEDA documentation overview.</p>
-    </>
-)
+import "../Docs.css"
+
+const OverviewDoc = () => {
+    
+    return (
+        <div>
+            <h1>Overview</h1>
+            <p>OSEDA documentation overview.</p>
+        </div>
+    )
+}
 
 export default OverviewDoc
