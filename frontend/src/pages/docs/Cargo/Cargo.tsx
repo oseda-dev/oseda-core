8428351d2d2d8d9a3db6f6a14ce276165f9145d9
ReeseHatfield <reesehatfield02@gmail.com>
2026-01-25 12:56:58 -0500

    add cargo docs

--- frontend/src/pages/docs/Cargo/Cargo.tsx
+++ frontend/src/pages/docs/Cargo/Cargo.tsx
@@ -1,8 +1,21 @@
-const Cargo = () => (
-    <>
-        <h1>Cargo</h1>
-        <p>idk</p>
-    </>
-)
+import "./Cargo.css"
+
+const Cargo = () => {
+
+    const title = "Oseda CLI Cargo Docs";
+    const docsUrl = "https://docs.rs/oseda-cli/latest/oseda_cli/"; 
+
+    return (
+        <>
+            <h1>{title}</h1>
+            <iframe
+                className="cargo-docs"
+                title={title}
+                src={docsUrl}
+                >
+            </iframe>
+        </>
+    )
+}
 
 export default Cargo
