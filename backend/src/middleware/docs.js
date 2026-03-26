6796cb721e88e3bcba59fe528f36d1b6de9e4088
ReeseHatfield <reesehatfield02@gmail.com>
2026-03-26 11:12:18 -0400

    fix doc paths since backend restructure

--- backend/src/middleware/docs.js
+++ backend/src/middleware/docs.js
@@ -15,29 +15,29 @@ const serveDocs = (server) => {
 
 
     // mount the readme at the top level
-    const cliReadmePath = path.join(__dirname, "../../../oseda-cli/README.md");
+    const cliReadmePath = path.join(__dirname, "../../../../oseda-cli/README.md");
     server.use("/api/docs/cli/", express.static(cliReadmePath))
 
-    const cliUsagePath = path.join(__dirname, "../../../oseda-cli/Usage.md");
+    const cliUsagePath = path.join(__dirname, "../../../../oseda-cli/Usage.md");
     server.use("/api/docs/cli/usage", express.static(cliUsagePath))
 
     
-    const courseContributing = path.join(__dirname, "../../docs/COURSE_CONTRIBUTING.md");
+    const courseContributing = path.join(__dirname, "../../../docs/COURSE_CONTRIBUTING.md");
     server.use("/api/docs/contributing/getting-started", express.static(courseContributing))
 
-    const codeOfConduct = path.join(__dirname, "../../docs/CODE_OF_CONDUCT.MD");
+    const codeOfConduct = path.join(__dirname, "../../../docs/CODE_OF_CONDUCT.MD");
     server.use("/api/docs/contributing/guidelines", express.static(codeOfConduct));
 
-    const frontendReadme = path.join(__dirname, "../../frontend/README.md");
+    const frontendReadme = path.join(__dirname, "../../../frontend/README.md");
     server.use("/api/docs/core/frontend", express.static(frontendReadme));
 
-    const backendReadme = path.join(__dirname, "../../backend/README.md");
+    const backendReadme = path.join(__dirname, "../../../backend/README.md");
     server.use("/api/docs/core/backend", express.static(backendReadme));
 
-    const coreReadme = path.join(__dirname, "../../README.md");
+    const coreReadme = path.join(__dirname, "../../../README.md");
     server.use("/api/docs/core/readme", express.static(coreReadme));
 
-    const libReadme = path.join(__dirname, "../../../oseda-lib/README.md");
+    const libReadme = path.join(__dirname, "../../../../oseda-lib/README.md");
     server.use("/api/docs/lib", express.static(libReadme));
 
 
