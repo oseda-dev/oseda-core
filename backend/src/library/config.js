16a4977ff5f4aba1fd728300565ef5bec6cddfc6
ReeseHatfield <reesehatfield02@gmail.com>
2026-08-30 09:33:21 -0400

    remove intentional error check

--- backend/src/library/config.js
+++ backend/src/library/config.js
@@ -39,8 +39,6 @@ const parse_config = (configPath) => {
     try {
         return JSON.parse(fs.readFileSync(configPath, "utf-8"));
     } catch (err) {
-        console.error(err);
-        // console.log("error state reached");
         throw { status: 500, message: "Failed to read config" };
     }
 }
